1918f6f3f8d7ac33f6c712f76196a39ad97eae88
ngothanhtan <oh.sick.cf@gmail.com>
2019-09-11 10:21:50 +0700

    list

---
+++ ArrayList.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class ArrayList
+{
+public $arrayList;
+public function ArrayList($arr = ''){
+    if (is_array($arr) == true){
+        $this->arrayList = $arr;
+    }else{
+        $this->arrayList = array();
+    }
+}
+public function add($obj){
+    array_push($this->arrayList, $obj);
+}
+public function get($index){
+    if ($this->isInteger($index)){
+        return $this->arrayList[$index];
+    }else{
+        die("ERROR in ArrayList.get");
+    }
+}
+public function isInteger($toCheck){
+    return preg_match("/^[0-9]+$/", $toCheck);
+}
+
+}
